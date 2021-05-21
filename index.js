df856d5a2613a90bf2b00b8f70a6e94b9903e208
noa soffer <noa.soffer@intel.com>
2021-05-21 11:35:28 +0300

    fix

--- index.js
+++ index.js
@@ -9,9 +9,18 @@ app.get('/', function(req, res){
     res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
-app.get('/puzzle', function(req, res){
-    res.sendFile(path.join(__dirname + '/public/add_puzzle_2.html'));
+app.get('/add_puzzle', function(req, res){
+    res.sendFile(path.join(__dirname + '/public/add_puzzle_3.html'));
 });
+
+app.get('/solver_view', function(req, res){
+    res.sendFile(path.join(__dirname + '/public/solver_view_2.html'));
+});
+
+app.get('/view_puzzles', function(req, res){
+    res.sendFile(path.join(__dirname + '/public/view_puzzles.html'));
+});
+
 
 app.get('/get_abi', function(req, res) {
     const data = fs.readFileSync(
